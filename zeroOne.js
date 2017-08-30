4ddd3f1685773ab10261b754beeefdb5706713d1
Shah Zafrani <szz786@gmail.com>
2017-08-30 19:26:18 -0400

    added simple gui based on DOM manipulation.

--- zeroOne.js
+++ zeroOne.js
@@ -4,10 +4,18 @@ const state2 = "STATE 2"; //acceptance state
 const rejState = "State 3";
 const fin = "Input Stream Accepted";
 const rej = "Input Stream Rejected: ";
-const err = "Input Stream Contains Invalid Character: ";
+const err = "Input Stream Rejected due to Invalid Character: ";
+
+var listDiv = null;
 
 function validateStream(inputString, currentState) {
   console.log("Current State is: " + currentState + ", Remaining Input Stream: " + inputString);
+  var node = document.createElement("li");
+  currentOutputText="Current State is: " + currentState + ", Remaining Input Stream: " + inputString;
+  listDiv.appendChild(node);
+  var text = document.createTextNode(currentOutputText);
+  node.appendChild(text);
+  
   if(inputString.length > 0) {
      switch(currentState) {
        case start: {
@@ -82,6 +90,17 @@ function validateStream(inputString, currentState) {
   }
 }
 
+function beginValidation(){
+  var input = document.getElementById("streamInput").value;
+  listDiv = document.getElementById("transitionList");
+  listDiv.innerHTML = '';
+  var output = validateStream(input.trim(), start);
+  var text = document.createTextNode(output);
+  var resultsNode = document.getElementById("results");
+  resultsNode.innerHTML = '';
+  resultsNode.appendChild(text);
+}
+
 //process.argv[2] is the first command line argument
-var input = process.argv[2];
-console.log(validateStream(input, start));
+// var input = process.argv[2];
+// console.log(validateStream(input, start));
